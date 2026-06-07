14bfa82dc6e48141ba7471073ddf6f71c94d9b8a
st <2663600842@qq.com>
2026-06-07 08:23:59 +0800

    feat：删除之前模板代码

--- internal/api/v1/qa/routes.go
+++
@@ -1,9 +0,0 @@
-package qa
-
-import "github.com/gin-gonic/gin"
-
-// RegisterRoutes 注册问答模块路由
-func (ctrl *Controller) RegisterRoutes(router *gin.RouterGroup) {
-	qaGroup := router.Group("/qa")
-	qaGroup.POST("/ask", ctrl.Ask)
-}
